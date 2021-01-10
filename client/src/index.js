5179e10b007c71ef1ac003208dd0bcd594009055
Greyson <greyson.r.hamilton@gmail.com>
2021-01-10 11:26:59 -0500

    store install

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
+import {Reducer} from './reducers/index';
 
 import "./index.css";
 import App from "./App";
 
+const store = createStore(Reducer, applyMiddleware(thunk));
+
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+    <App />
+    </Provider>, 
     rootElement
 );
 
 //Task List:
-//1. Add in all necessary components and libary methods.
+//1. Add in all necessary components and library methods.
 //2. Create a store that includes thunk middleware support.
 //3. Wrap the App component in a react-redux Provider element.
